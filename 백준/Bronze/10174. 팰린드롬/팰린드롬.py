04e0a9e80df4e7d9523247ff4e03e7891892c3bf
signkite <50399725+signkite@users.noreply.github.com>
2023-01-16 17:13:53 +0900

    [Bronze II] Title: 팰린드롬, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10174. 팰린드롬/팰린드롬.py
+++ 백준/Bronze/10174. 팰린드롬/팰린드롬.py
@@ -1,9 +1,8 @@
 n = int(input())
 for _ in range(n):
     text = input().lower()
-    rev_text = text[-1::-1]
+    rev_text = text[::-1]
     if text == rev_text:
         print("Yes")
     else:
         print("No")
-    
