3ee4e9a1a618dbc4d03dad6adde83e1a61e16af4
signkite <50399725+signkite@users.noreply.github.com>
2023-12-28 03:56:20 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 192 ms, Memory: 62,904 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -1,6 +1,15 @@
-from collections import Counter
 T = int(input())
-for _ in range(1, T + 1):
-    test_case = int(input())
-    counter = Counter(list(map(int, input().split())))
-    print(f'#{test_case} {counter.most_common()[0][0]}')
+
+for _ in range(T):
+    test_case = input()
+    counter = {}
+    for num in map(int, input().split()):
+        if num not in counter:
+            counter[num] = 0
+        counter[num] += 1
+
+    # [(숫자, 개수), (숫자, 개수), ... ] 로 구성된 배열을
+    # 개수 기준 오름차순으로, 개수가 같다면 숫자 기준 오름차순으로 정렬
+    items = sorted(list(counter.items()), key=lambda x: (x[1], x[0]))
+    print(f"#{test_case} {items[-1][0]}")
+    
