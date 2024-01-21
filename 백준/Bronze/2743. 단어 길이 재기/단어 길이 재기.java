959fa79affbb162008b5501e4e871668afa5287e
signkite <50399725+signkite@users.noreply.github.com>
2024-01-21 23:38:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 128 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println((new BufferedReader(new InputStreamReader(System.in))).readLine().length());
+    }
+}
